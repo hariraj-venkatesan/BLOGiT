18ea2d10f5f9d1aab0badbba5b651bdbd9db5164
hariraj <hariraj.venkatesan@freshworks.com>
2019-08-29 15:31:39 +0530

    Completed user edit and update

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,17 +3,37 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   setup do
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
+  # test "should redirect index when not logged in" do
+  #   log_out_user
+  #   get :index
+  #   assert_redirected_to login_url
+  # end
+
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:users)
+  # end
 
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name,
+                                      email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
   # test "should create user" do
@@ -30,6 +50,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    log_in_as(@user)
     get :edit, id: @user
     assert_response :success
   end
@@ -45,5 +66,19 @@ class UsersControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to users_path
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
